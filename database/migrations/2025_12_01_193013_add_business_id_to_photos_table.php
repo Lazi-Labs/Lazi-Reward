02b1cbeaf48b54888af42683d1410aa10b0a8b02
portseif <chris@portseif.io>
2025-12-01 22:32:51 -0500

    Add `Review` and `Photo` resources with migrations and Filament actions, and link photos to businesses

---
+++ database/migrations/2025_12_01_193013_add_business_id_to_photos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Clear existing photos without business_id
+        DB::table('photos')->truncate();
+
+        Schema::table('photos', function (Blueprint $table) {
+            $table->foreignUuid('business_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('business_id');
+        });
+    }
+};
